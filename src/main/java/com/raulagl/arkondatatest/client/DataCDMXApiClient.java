9f3b3f7cdd404917c609ae36c474ef40825b7293
raulagl <Male2583>
2021-10-04 01:28:07 -0500

    * consume api of 'alcaldias' from data of cdmx

--- src/main/java/com/raulagl/arkondatatest/client/DataCDMXApiClient.java
+++ src/main/java/com/raulagl/arkondatatest/client/DataCDMXApiClient.java
@@ -1,7 +1,7 @@
 package com.raulagl.arkondatatest.client;
 
 import com.raulagl.arkondatatest.dto.ResponseDataCDMXDTO;
-
+import com.raulagl.arkondatatest.dto.TownHallDTO;
 import com.raulagl.arkondatatest.dto.VehicleDTO;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
@@ -12,6 +12,7 @@ public interface DataCDMXApiClient {
     @Get( "/datastore_search_sql?sql=SELECT%20*%20from%20%22ad360a0e-b42f-482c-af12-1fd72140032e%22" )
     ResponseDataCDMXDTO<VehicleDTO> getDataOfBuses();
 
-
+    @Get( "/datastore_search_sql?sql=SELECT%20*%20from%20%22dbb00cee-3660-43f6-89c2-8beb433292a8%22" )
+    ResponseDataCDMXDTO<TownHallDTO> getDataOfTownHall();
 }
 
---
+++ src/main/java/com/raulagl/arkondatatest/dto/TownHallDTO.java
@@ -0,0 +1,42 @@
+package com.raulagl.arkondatatest.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TownHallDTO {
+
+
+    @JsonProperty( "_id" )
+    private Integer idProvided;
+
+    @JsonProperty( "_full_text" )
+    private String fullText;
+
+    @JsonProperty( "id" )
+    private Integer id;
+
+    @JsonProperty( "nomgeo" )
+    private String nameTownHall;
+
+    @JsonProperty( "cve_mun" )
+    private Integer keyMun;
+
+    @JsonProperty( "cve_ent" )
+    private Integer keyEnt;
+
+    @JsonProperty( "cvegeo" )
+    private Integer keyGeo;
+
+    @JsonProperty( "gid" )
+    private Integer gid;
+
+    @JsonProperty( "geo_point_2d" )
+    private String geographicPoint2d;
+
+    @JsonProperty( "geo_shape" )
+    private String geographicShape;
+
+}
