5d0f919d98ceb49a4cf6319b12d8d26a5bc7e62f
raulagl <Male2583>
2021-10-05 23:05:11 -0500

    * method is added to find units by current status

--- src/main/java/com/raulagl/arkondatatest/repositories/VehicleRepository.java
+++ src/main/java/com/raulagl/arkondatatest/repositories/VehicleRepository.java
@@ -4,5 +4,10 @@ import com.raulagl.arkondatatest.domain.Vehicle;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> { }
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    List<Vehicle> findByVehicleCurrentStatus(Integer status);
+}
